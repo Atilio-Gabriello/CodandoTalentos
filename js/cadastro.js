cabe36533817483dfe6063732ba9bf47bda8d30a
Atilio-Gabriello <atilioga22@gmail.com>
2021-05-21 09:45:39 -0300

    Continuando o edit

---
+++ js/cadastro.js
@@ -0,0 +1,10 @@
+let i = 0
+document.getElementById("botao1").onclick = function(){
+    console.log(i)
+    i++
+}
+
+document.getElementById("botao2").onclick = function(){
+    console.log(i)
+    i--
+}
