17c8a2f3cd4e7bbd556ae3bfa27e1dbaa6a5e721
Atilio-Gabriello <atilioga22@gmail.com>
2021-05-24 09:08:29 -0300

    Editar/remover finalizado

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,10 +1,34 @@
-let i = 0
-document.getElementById("botao1").onclick = function(){
-    console.log(i)
-    i++
+var modal1 = document.getElementById("modal1");
+var modal2 = document.getElementById("modal2");
+
+var btn1 = document.getElementById("botao1");
+var btn2 = document.getElementById("botao2");
+
+var span1 = document.getElementsByClassName("close1")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+
+btn1.onclick = function(){
+    modal1.style.display = "block";
 }
 
-document.getElementById("botao2").onclick = function(){
-    console.log(i)
-    i--
+btn2.onclick = function(){
+    modal2.style.display = "block";
+}
+
+span1.onclick = function(){
+    console.log("ta indo")
+    modal1.style.display = "none";
+}
+
+span2.onclick = function(){
+    modal2.style.display = "none";
+}
+
+window.onclick = function(event){
+    if(event.target == modal1){
+        modal1.style.display = "none";
+    }
+    else if(event.target == modal2){
+        modal2.style.display = "none";
+    }
 }
